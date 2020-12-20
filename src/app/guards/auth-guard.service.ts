7848ba01d8eef9ddf3ca2be9d522d6304deb16cd
ashminbhandari <abhandar@ramapo.edu>
2020-12-19 19:12:07 -0500

    🌱 lints

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -7,8 +7,6 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-
-
   constructor(private authService: AuthService, private router: Router) {
   }
 
@@ -16,7 +14,6 @@ export class AuthGuardService implements CanActivate {
     if (this.authService.isAuthenticated()) {
       return true;
     }
-    // you can save redirect url so after authing we can move them back to the page they requested
     return false;
   }
 
