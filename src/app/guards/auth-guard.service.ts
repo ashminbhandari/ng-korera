d8c9207659a3d51cce72eb238d657f247ae237a3
ashminbhandari <abhandar@ramapo.edu>
2020-12-19 19:10:14 -0500

    🔒 auth guard/authentication/routing setup 🔒

---
+++ src/app/guards/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { AuthService } from './../services/auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    // you can save redirect url so after authing we can move them back to the page they requested
+    return false;
+  }
+
+}
